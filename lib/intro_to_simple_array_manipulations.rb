59b06b8d1a3b757008af0632fde99141954a4896
zed <zed@kolk.me>
2020-12-14 22:53:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,21 +4,21 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift
-  
+
 end
 
 def using_pop
-  
+
 end
 
 def pop_with_args
-  
+
 end
 
 def using_shift
-  
+
 end
 
 def shift_with_args
-  
+
 end
