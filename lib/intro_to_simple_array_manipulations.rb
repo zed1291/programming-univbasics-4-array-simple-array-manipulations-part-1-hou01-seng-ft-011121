6102c884262f853981a71c0274ed7c7b2ce9f35b
zed <zed@kolk.me>
2020-12-14 22:52:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,4 @@
-def updated_array(colors_in_the_rainbow, next_color)
+def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow = colors_in_the_rainbow.push(next_color)
   colors_in_the_rainbow
 end
